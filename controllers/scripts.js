2ca41e2c52b9fa3c2a8fd8beb94d6b9d46cf3940
ttakkula <ttakkula@gmail.com>
2015-10-27 15:14:01 +0200

    JS & jquery rehearsal

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,1 +1,41 @@
+//Wait document ready event
+$(document).ready(function(){
+  console.log("jquery onload triggered");
+    $("#head").css("background-color","lightblue")
+    .css("padding","2em").css("border-radius","8px");
+    $(".about").css("text-align","center");
+    $(".about").html("<em>New text</em>");
+    $("[data-dummy]").css("transform","rotate(20deg)");
+    $("[data-dummy]").html("<p>Hello World</p>");
+});
+
+//Sama kuin edellä, mutta nimetyn funktion kanssa - edellä oleva anonyymi tapa on lyhyempi ja useimmin käytetty
+/*
+$(document).ready(domReady);
+function domReady(){
+    
+}
+*/
+
+/*
 console.log("Here we go!");
+
+window.onload = function(event){
+    console.log(event);
+    para1.innerHTML = "Changed from JS";
+    para1.style.backgroundColor = "yellow";
+}
+
+window.onload = domReady;
+
+function domReady(event){
+    return 2;
+}
+
+function someFunction(nimi){
+    console.log(nimi);
+}
+
+someFunction();
+someFunction("Timo");
+*/
