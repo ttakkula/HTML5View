d3cfdfd484eed694299729dbaefa2a92d57eebac
ttakkula <ttakkula@gmail.com>
2015-10-28 13:01:29 +0200

    Data import from MongoDB with AJAX and Bootstrap styling exercise

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -7,6 +7,41 @@ $(document).ready(function(){
     $(".about").html("<em>New text</em>");
     $("[data-dummy]").css("transform","rotate(20deg)");
     $("[data-dummy]").html("<p>Hello World</p>");
+    
+    var setting = {
+        method:"GET",
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp"
+    }
+    $.ajax(setting).done(function(data){
+        console.log(data);
+        for(i=2; i < data.rows.length; i++){
+            
+            var html = "<tr>" +
+                       "<td>" + data.rows[i].name + "</td>" +
+                       "<td>" + data.rows[i].address + "</td>" +
+                       "<td>" + data.rows[i].age + "</td>" +
+                       "</tr>";
+            $(html).appendTo("tbody");
+        }
+        for(i=0; i < data.rows.length; i++){
+            
+            var html = "<h3>Name <span class='label label-primary'>" + data.rows[i].name + "</span></h3>" +
+                       "<h3>Address <span class='label label-primary'>" + data.rows[i].address + "</span></h3>" +
+                       "<h3>Age <span class='label label-primary'>" + data.rows[i].age + "</span></h3><hr>";
+            $(html).appendTo("#otsikot");
+        }
+        var i = 0;
+        while(i < 3){
+            var top3 = "<h3>Name <span class='label label-primary'>" + data.rows[i].name + "</span></h3>" +
+            "<h3>Address <span class='label label-primary'>" + data.rows[i].address + "</span></h3>" +
+            "<h3>Age <span class='label label-primary'>" + data.rows[i].age + "</span></h3><hr>";
+            i++;
+            $(top3).appendTo("#topthree");
+        }            
+    }
+    );
 });
 
 //Sama kuin edellä, mutta nimetyn funktion kanssa - edellä oleva anonyymi tapa on lyhyempi ja useimmin käytetty
